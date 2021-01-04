ad6571b4e4facf70bf130294f7fec1dd00a73d86
dinasamer559@yahoo.com <dinasamer6242@gmail.com>
2021-01-04 13:55:50 +0200

    ngx translation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'CDAD';
+
+  constructor(public translate: TranslateService) {
+    // const lang = localStorage.getItem('language');
+    // if (lang) {
+    //   translate.setDefaultLang(lang);
+    //   translate.use(lang);
+    // } else {
+    //   translate.setDefaultLang('en');
+    //   translate.use('en');
+    //   localStorage.setItem('language', 'en');
+    // }
+  }
+
+  isEn = true;
+
+  useLanguage(): void {
+    this.isEn = !this.isEn;
+    // this.isEn ? this.translate.use('en') : this.translate.use('ar');
+    // const currentlang = this.translate.currentLang;
+    // console.log(currentlang);
+    if (!this.isEn) {
+      localStorage.setItem('language', 'ar');
+      // this.translate.setDefaultLang('ar');
+      this.translate.use('ar');
+    } else {
+      localStorage.setItem('language', 'en');
+      // this.translate.setDefaultLang('en');
+      this.translate.use('en');
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClient , HttpClientModule } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: createTranslateLoader,
+        deps: [HttpClient],
+      },
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+}
+
+export function createTranslateLoader(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
