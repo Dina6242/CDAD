d141a4f092a4490baa01f528b8853675744ba440
dinasamer559@yahoo.com <dinasamer6242@gmail.com>
2021-01-16 20:55:15 +0200

    change btn depend on url and lang

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { LocalizeRouterService } from '@gilsdav/ngx-translate-router';
-import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-home',
@@ -10,23 +9,16 @@ import { TranslateService } from '@ngx-translate/core';
 export class HomeComponent implements OnInit {
   isEn = true;
 
-  constructor(public localizeService: LocalizeRouterService, public translate: TranslateService) {
-  }
+  constructor(public localizeService: LocalizeRouterService) {}
 
   ngOnInit(): void {
+    if (this.localizeService.parser.currentLang === 'ar') {
+      this.isEn = false;
+    }
+
     this.localizeService.routerEvents.subscribe((language: string) => {
-      console.log(language);
+      this.isEn = language === 'en';
     });
   }
 
-  changebtn(): void {
-    this.isEn = !this.isEn;
-    if (!this.isEn) {
-      this.translate.setDefaultLang('ar');
-      this.translate.use('ar');
-    } else {
-      this.translate.setDefaultLang('en');
-      this.translate.use('en');
-    }
-  }
 }
